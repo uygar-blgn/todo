d1a51aafbe7151a6678c85dbaa24ca56b25461da
uygar-blgn <uygarblgn13@outlook.com>
2024-03-08 13:26:32 +0300

    gitignore

--- src/Task.js
+++ src/Task.js
@@ -1,7 +1,7 @@
 class Task {
 
-    constructor(title='', desc='', priority='Low Priority', due='01/01/1970', complete=false) {
-        this.title = title
+    constructor(name='', desc='', priority='Low Priority', due='01/01/1970', complete=false) {
+        this.name = name
         this.desc = desc
         this.priority = priority
         this.complete = complete
