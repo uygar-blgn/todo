4cef83599332c314dbd32388156a61148084f4b6
uygar-blgn <uygarblgn13@outlook.com>
2024-03-08 14:32:04 +0300

    finish skeleton functionality

--- src/Display.js
+++ src/Display.js
@@ -73,11 +73,44 @@ class Display {
 
                 const taskDelete = document.createElement('div')
                 taskDelete.className = 'task-delete'
+                const deleteImg = document.createElement('img')
+                deleteImg.src = 'https://www.svgrepo.com/show/124986/big-trash-can.svg'
+                taskDelete.appendChild(deleteImg)
 
                 taskName.textContent = todo.name
                 taskDesc.textContent = todo.desc
                 taskPriority.textContent = todo.priority
                 taskDue.textContent = todo.due
+
+                if(!todo.complete) {
+                    const checkboxImg = document.createElement('img')
+                    checkboxImg.src = 'https://www.svgrepo.com/show/309415/checkbox-unchecked.svg'
+                    taskComplete.appendChild(checkboxImg)
+                }
+                else {
+                    const checkboxImg = document.createElement('img')
+                    checkboxImg.src = 'https://www.svgrepo.com/show/309414/checkbox-checked.svg'
+                    taskComplete.appendChild(checkboxImg)
+                }
+
+                taskComplete.addEventListener('click', () => {
+                    if(!todo.complete) {
+                        todo.complete = true
+                        taskComplete.querySelector('img').src = 'https://www.svgrepo.com/show/309414/checkbox-checked.svg'
+                        task.style.backgroundColor = 'rgb(200, 200, 200)'
+                    }
+                    else {
+                        todo.complete = false
+                        taskComplete.querySelector('img').src = 'https://www.svgrepo.com/show/309415/checkbox-unchecked.svg'
+                        task.style.backgroundColor = 'white'
+                    }
+                })
+
+                taskDelete.addEventListener('click', () => {
+                    const taskIndex = project.todos.indexOf(todo)
+                    project.todos.splice(taskIndex, 1)
+                    this.renderContent()
+                })
                 
                 task.appendChild(taskName)
                 task.appendChild(taskDesc)
@@ -89,6 +122,86 @@ class Display {
                 projectCategory.appendChild(task)
             })
 
+            const newTask = document.createElement('div')
+            newTask.id = 'new-task'
+            const newTaskImg = document.createElement('img')
+            newTaskImg.src = 'https://www.svgrepo.com/show/2087/plus.svg'
+            newTask.appendChild(newTaskImg)
+
+            newTask.addEventListener('click', () => {
+                const newTaskName = prompt('Task name?')
+                const newTaskDesc = prompt('Task description?')
+                const newTaskPriority = prompt('Task priority?')
+                const newTaskDue = prompt('Due date?')
+                const newTaskObj = new Task(newTaskName, newTaskDesc, newTaskPriority, newTaskDue, false)
+                project.todos.push(newTaskObj)
+                const addedTask = document.createElement('div')
+                addedTask.className = 'task'
+
+                const taskName = document.createElement('div')
+                taskName.className = 'task-name'
+
+                const taskDesc = document.createElement('div')
+                taskDesc.className = 'task-desc'
+
+                const taskPriority = document.createElement('div')
+                taskPriority.className = 'task-priority'
+
+                const taskDue = document.createElement('div')
+                taskDue.className = 'task-due'
+
+                const taskComplete = document.createElement('div')
+                taskComplete.className = 'task-complete'
+
+                const taskDelete = document.createElement('div')
+                taskDelete.className = 'task-delete'
+                const deleteImg = document.createElement('img')
+                deleteImg.src = 'https://www.svgrepo.com/show/124986/big-trash-can.svg'
+                taskDelete.appendChild(deleteImg)
+
+                taskName.textContent = newTaskName
+                taskDesc.textContent = newTaskDesc
+                taskPriority.textContent = newTaskPriority
+                taskDue.textContent = newTaskPriority
+
+
+                const checkboxImg = document.createElement('img')
+                checkboxImg.src = 'https://www.svgrepo.com/show/309415/checkbox-unchecked.svg'
+                taskComplete.appendChild(checkboxImg)
+
+                taskComplete.addEventListener('click', () => {
+                    if(!newTaskObj.complete) {
+                        newTaskObj.complete = true
+                        checkboxImg.src = 'https://www.svgrepo.com/show/309414/checkbox-checked.svg'
+                        addedTask.style.backgroundColor = 'rgb(200, 200, 200)'
+                    }
+                    else {
+                        newTaskObj.complete = false
+                        checkboxImg.src = 'https://www.svgrepo.com/show/309415/checkbox-unchecked.svg'
+                        addedTask.style.backgroundColor = 'white'
+                    }
+                })
+
+                taskDelete.addEventListener('click', () => {
+                    const taskIndex = project.todos.indexOf(newTaskObj)
+                    project.todos.splice(taskIndex, 1)
+                    this.renderContent()
+                })
+                
+                addedTask.appendChild(taskName)
+                addedTask.appendChild(taskDesc)
+                addedTask.appendChild(taskPriority)
+                addedTask.appendChild(taskDue)
+                addedTask.appendChild(taskComplete)
+                addedTask.appendChild(taskDelete)
+
+                projectCategory.removeChild(newTask)
+                projectCategory.appendChild(addedTask)
+                projectCategory.appendChild(newTask)
+            })
+
+
+            projectCategory.appendChild(newTask)
             content.appendChild(projectCategory)
         })
     }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import { Task } from "./Task"
 import { Display } from "./Display"
 import { Project } from "./Project"
 
-const task1 = new Task('task1', 'yes', 'High Priority', '01/07/2004', false)
+const untitled = new Project('', [])
 
-const untitled = new Project('', [task1])
-const a = new Project('a', [task1])
-const b = new Project('b', [task1])
-
-new Display([untitled, a, b])
+new Display([untitled])
